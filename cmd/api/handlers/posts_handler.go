8cfe6abbb115cecc03a38a71db2e4760aaed5b8f
suntoryota <ryotasunto@gmail.com>
2022-12-26 21:48:42 +0700

    go echo crud api

---
+++ cmd/api/handlers/posts_handler.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"go-echo/cmd/api/service"
+
+	"github.com/labstack/echo/v4"
+)
+
+func PostIndexHandler(c echo.Context) error {
+	data, err := service.GetAll()
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+	return c.JSON(http.StatusOK, res)
+}
+
+func PostSingleHandler(c echo.Context) error {
+	id := c.Param("id")
+	idx, err := strconv.Atoi(id)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	data, err := service.GetById(idx)
+	if err != nil {
+		c.String(http.StatusBadGateway, "Unable to process data")
+	}
+	res := make(map[string]any)
+	res["status"] = "ok"
+	res["data"] = data
+	return c.JSON(http.StatusOK, res)
+}
